0c9eeae76fca320ef75a32670fc7e8ee6faf3022
abkap <apabhishek2002@gmail.com>
2023-07-05 22:33:49 +0530

    added no route found warning if unreahced route

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="works" element={<ProjectsPage />} />
+      <Route
+        path="*"
+        element={<div className="text-white">No route found</div>}
+      />
     </Routes>
   );
 }
